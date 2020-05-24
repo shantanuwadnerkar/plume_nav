9c1c7ccbd78e7fd4f3981b1fa5c9a890ee85e38d
Shantanu Wadnerkar <shantanu5996@gmail.com>
2020-05-23 19:13:28 -0700

    Remove RotorS from the build config

--- build.sh
+++ build.sh
@@ -2,14 +2,18 @@
 
 cd src/
 
+# GADEN
 git clone git@github.com:shantanuwadnerkar/gaden.git
-git clone git@github.com:shantanuwadnerkar/rotors_simulator.git
 git clone git@github.com:MAPIRlab/olfaction_msgs.git
-git clone git@github.com:ethz-asl/mav_comm.git
+
+# RotorS
+# git clone git@github.com:shantanuwadnerkar/rotors_simulator.git
+# git clone git@github.com:ethz-asl/mav_comm.git
 
 cd ..
 
-catkin build
+# change the build tool to catkin-tools
+catkin_make
 
-printf "\n\n\n\n\n\n\n\n\n"
+# printf "\n\n\n\n\n\n\n\n\n"
 echo "PROJECT BUILT!!"
