958d35426053df879c0395c45438ed0bab1fac5f
Aravind Sreejith <aravindrsnair@gmail.com>
2020-05-28 11:14:00 -0700

    Better implementation of cmd_vel; Rate can be anything from 2-100

--- src/plume/crazyflie_description/src/pose_transform.cpp
+++ src/plume/crazyflie_description/src/pose_transform.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <cstdlib>
 
 #include <ros/ros.h>
 #include <geometry_msgs/TransformStamped.h>
@@ -19,17 +20,9 @@ class VelocityTransform
         double y_vel{0.0};
         double z_vel{0.0};
         double yaw_rate{0.0};
-        double x{0.0};
-        double y{0.0};
-        double z{0.5};
-        double yaw{0.0};
-        ros::Time current_time = ros::Time::now();
-        ros::Time last_time = ros::Time::now();
+        ros::Time message_time = ros::Time::now() - ros::Duration(5);
 
     private:
-        double dx, dy, dtheta;
-        double dt = 0.1;
-        int first_time = 1;
 };
 
 VelocityTransform::VelocityTransform()
@@ -41,27 +34,11 @@ VelocityTransform::~VelocityTransform()
 void VelocityTransform::cmd_vel_callback(const geometry_msgs::Twist::ConstPtr& msg)
 {
     ROS_INFO("OOP");
-    // current_time = ros::Time::now();
-    // if (first_time) 
-    // {
-    //     last_time = current_time;
-    //     first_time = 0;
-    // }
     x_vel = msg->linear.x;
     y_vel = msg->linear.y;
     z_vel = msg->linear.z;
     yaw_rate = msg->angular.z;
-
-    // dt = (current_time-last_time).toSec();
-    dx = (x_vel * cos(yaw) - y_vel * sin(yaw)) * dt;
-    dy = (x_vel * sin(yaw) + y_vel * cos(yaw)) * dt;
-    dtheta = yaw_rate * dt;
-
-    x += dx;
-    y += dy;
-    yaw += dtheta;
-
-    // last_time = current_time;
+    message_time = ros::Time::now();
 }
 
 
@@ -69,7 +46,7 @@ int main(int argc, char** argv) {
     ros::init(argc, argv, "pose_transform");
     ros::NodeHandle n;
 
-    VelocityTransform velocityTransform;
+    VelocityTransform vT;
 
     tf::TransformBroadcaster mapToOdom_;
     tf::Transform map_transform_{ tf::Quaternion(0, 0, 0, 1), tf::Vector3(0.0, 0.0, 0.0) };
@@ -78,10 +55,23 @@ int main(int argc, char** argv) {
     geometry_msgs::TransformStamped odom_transform;
     nav_msgs::Odometry odom;
 
-    ros::Subscriber cmd_vel_sub = n.subscribe("cmd_vel", 1, &VelocityTransform::cmd_vel_callback, &velocityTransform);
+    ros::Subscriber cmd_vel_sub = n.subscribe("cmd_vel", 1, &VelocityTransform::cmd_vel_callback, &vT);
     ros::Publisher odom_pub = n.advertise<nav_msgs::Odometry>("base_pose_ground_truth",10);
 
     ros::Rate rate(100);
+
+    double dt = 0.01;
+    double timeout = 0.55;
+    double dx, dy, dtheta;
+
+    ros::Time current_time;
+    ros::Time previous_time;
+
+    // Following variables should be initialized from parameters
+    double x{0.0};
+    double y{0.0};
+    double z{0.5};
+    double yaw{0.0};
 
     odom_transform.header.frame_id = "odom";
     odom_transform.child_frame_id = "base_link";
@@ -90,31 +80,45 @@ int main(int argc, char** argv) {
 
     while (ros::ok())
     {
-        mapToOdom_.sendTransform(tf::StampedTransform(map_transform_, velocityTransform.current_time, "map", "odom"));        
+        current_time = ros::Time::now();
 
-        // ROS_INFO("%.2f, %.2f, %.2f", velocityTransform.x, velocityTransform.y, velocityTransform.z);
+        mapToOdom_.sendTransform(tf::StampedTransform(map_transform_, current_time, "map", "odom"));        
         
-        odom_transform.header.stamp = velocityTransform.current_time;
-        odom_transform.transform.translation.x = velocityTransform.x;
-        odom_transform.transform.translation.y = velocityTransform.y;
-        odom_transform.transform.translation.z = velocityTransform.z;
-        odom_transform.transform.rotation = tf::createQuaternionMsgFromYaw(velocityTransform.yaw);
+        if (abs((current_time - vT.message_time).toSec()) >= timeout)
+        {
+            vT.x_vel = 0;
+            vT.y_vel = 0;
+            vT.yaw_rate = 0;
+        }
+        dx = (vT.x_vel * cos(yaw) - vT.y_vel * sin(yaw)) * dt;
+        dy = (vT.x_vel * sin(yaw) + vT.y_vel * cos(yaw)) * dt;
+        dtheta = vT.yaw_rate * dt;
+
+        x += dx;
+        y += dy;
+        yaw += dtheta;        
+        
+        odom_transform.header.stamp = current_time;
+        odom_transform.transform.translation.x = x;
+        odom_transform.transform.translation.y = y;
+        odom_transform.transform.translation.z = z;
+        odom_transform.transform.rotation = tf::createQuaternionMsgFromYaw(yaw);
 
         odomTransform_.sendTransform(odom_transform);
 
         // Filling odometry
         // Setting position
-        odom.header.stamp = velocityTransform.current_time;
-        odom.pose.pose.position.x = velocityTransform.x;
-        odom.pose.pose.position.y = velocityTransform.y;
-        odom.pose.pose.position.z = velocityTransform.z;
-        odom.pose.pose.orientation = tf::createQuaternionMsgFromYaw(velocityTransform.yaw);
+        odom.header.stamp = current_time;
+        odom.pose.pose.position.x = x;
+        odom.pose.pose.position.y = y;
+        odom.pose.pose.position.z = z;
+        odom.pose.pose.orientation = tf::createQuaternionMsgFromYaw(yaw);
 
         // Setting velocity
-        odom.twist.twist.linear.x = velocityTransform.x_vel;
-        odom.twist.twist.linear.y = velocityTransform.y_vel;
+        odom.twist.twist.linear.x = vT.x_vel;
+        odom.twist.twist.linear.y = vT.y_vel;
         odom.twist.twist.linear.z = 0.0;
-        odom.twist.twist.angular.z = velocityTransform.yaw_rate;
+        odom.twist.twist.angular.z = vT.yaw_rate;
 
         odom_pub.publish(odom);
 
