bca8737f60d020e6f28ad6101550cfde4f2e173d
Aravind Sreejith <aravindrsnair@gmail.com>
2020-05-31 21:34:25 -0700

    compute velocity function

--- src/plume/crazyflie_control/scripts/move_drone.py
+++ src/plume/crazyflie_control/scripts/move_drone.py
@@ -27,6 +27,10 @@ class MoveDrone:
         self.goal_y = 0.0
 
         self._position_epsilon = 1e-2
+        self._angle_epsilon = 1e-2
+
+        self._velocity_gain = 2
+        self._angular_gain = 0.5
 
         self.actionlib_server = actionlib.SimpleActionServer('waypoint_heuristic', waypointAction, self.waypoint_callback, False)
         self.actionlib_server.start()
@@ -40,29 +44,33 @@ class MoveDrone:
 
 
     def compute_velocity_to_goal(self):
-        # goal_distance = math.sqrt((self.drone_curr_x - self.goal_x) ** 2 + (self.drone_curr_y - self.goal_y) ** 2)
-        # if self.has_reached_goal():
-        #     angle_to_goal = math.atan2((goal_y - y), (goal_x - x))
-        #     if abs(angle_to_goal - theta) > self._position_epsilon:
-        #         vel_msg.linear.x = 0
-        #         vel_msg.angular.z = 2 * self.angular_difference(theta, angle_to_goal)
-        #     else:
-        #         vel_msg.linear.x = 0.5 * goal_distance
-        #         vel_msg.angular.z = 0
-        #     return False
-        # else:
-        #     vel_msg.linear.x = 0
-        #     vel_msg.angular.z = 0
-        #     return True
-        pass
-
-
-    def has_reached_goal(self):
-        # Use the below formula or do it using goal_distance. Decide how.
-        if (abs(self.goal_x - self.drone_curr_x) < self._position_epsilon) and (abs(self.goal_y - self.drone_curr_y) < self._position_epsilon):
-            return True
-        else:
+        
+        goal_distance = math.sqrt((self.drone_curr_x - self.goal_x) ** 2 + (self.drone_curr_y - self.goal_y) ** 2)
+        
+        if goal_distance > self._position_epsilon:
+        
+            angle_to_goal = math.atan2((self.goal_y - self.drone_curr_y), (self.goal_x - self.drone_curr_x))
+        
+            if abs(angle_to_goal - theta) > self._angle_epsilon:
+                vel_msg.linear.x = 0
+                vel_msg.angular.z = self._velocity_gain * self.angular_difference(theta, angle_to_goal)
+            else:
+                vel_msg.linear.x = self._angular_gain * goal_distance
+                vel_msg.angular.z = 0
             return False
+        
+        else:            
+            vel_msg.linear.x = 0
+            vel_msg.angular.z = 0
+            return True
+
+    # Removing the following function because the same calculation is done right before calling this function
+    # def has_reached_goal(self):
+    #     # Use the below formula or do it using goal_distance. Decide how.
+    #     if math.sqrt( (self.goal_x - self.drone_curr_x)**2 + (self.goal_y - self.drone_curr_y)**2 ) < self._position_epsilon:
+    #         return True
+    #     else:
+    #         return False
 
 
 
